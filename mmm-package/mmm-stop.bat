493a642443d3e8f86eb47e2fbd789705acb5f42b
763830735 <763830735@qq.com>
2024-09-07 19:38:19 +0800

    去掉中文

--- mmm-package/mmm-stop.bat
+++ mmm-package/mmm-stop.bat
@@ -3,7 +3,7 @@ chcp 65001
 set port=14569
 for /f "tokens=1-5" %%i in ('netstat -ano^|findstr ":%port%"') do (
     echo kill the process %%m who use the port %port%
-    echo 正在关闭,请等待 %%m
+    echo please waiting %%m
     taskkill /f /pid %%m
 )
 Pause
